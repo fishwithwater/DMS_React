cc697ede096d0376781a6bf6c7182b0b500b8b91
maoyingjie <yingjie.mao@walkyren.com>
2019-03-21 19:01:12 +0800

    api update

--- src/config/api.config.js
+++ src/config/api.config.js
@@ -12,6 +12,15 @@ const API = {
     add_material: DOMAIN + '/material/add',
     update_material: DOMAIN + '/material/update',
     update_material_status: DOMAIN + '/material/able',
+    search_material_like_key: DOMAIN + '/material/search-like',
+    list_template_pagination: DOMAIN + '/template/list-template-pagination',
+    add_template: DOMAIN + '/template/add-template',
+    update_template: DOMAIN + '/template/update-template',
+    delete_template: DOMAIN + '/template/delete-template',
+    list_template_config_pagination: DOMAIN + '/template/list-template-config-pagination',
+    add_template_config: DOMAIN + '/template/add-template-config',
+    update_template_config: DOMAIN + '/template/update-template-config',
+    delete_template_config: DOMAIN + '/template/delete-template-config',
 }
 
 export {
